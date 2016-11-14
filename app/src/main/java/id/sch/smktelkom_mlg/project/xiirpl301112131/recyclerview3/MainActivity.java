eaa24dc6337ee62d394632e499527fe85f361d96
Vergie Ericson T <vergieericsont@gmail.com>
2016-11-14 19:38:22 +0700

    RecyclerView List Card

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl301112131/recyclerview3/MainActivity.java
@@ -0,0 +1,86 @@
+package id.sch.smktelkom_mlg.project.xiirpl301112131.recyclerview3;
+
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.project.xiirpl301112131.recyclerview3.adapter.HotelAdapter;
+import id.sch.smktelkom_mlg.project.xiirpl301112131.recyclerview3.model.Hotel;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Hotel> mList = new ArrayList<>();
+    HotelAdapter mAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new HotelAdapter(mList);
+        recyclerView.setAdapter(mAdapter);
+        fillData();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void fillData() {
+        Resources resources = getResources();
+        String [] arJudul = resources.getStringArray(R.array.places);
+        String [] arDeskripsi = resources.getStringArray(R.array.place_desc);
+        TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+        Drawable[] arFoto = new Drawable[a.length()];
+        for(int i = 0; i < arFoto.length;i++){
+            arFoto[i] = a.getDrawable(i);
+        }
+        a.recycle();
+        for (int i = 0; i <arJudul.length;i++){
+            mList.add(new Hotel(arJudul[i],arDeskripsi[i],arFoto[i]));
+        }
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl301112131/recyclerview3/model/Hotel.java
@@ -0,0 +1,16 @@
+package id.sch.smktelkom_mlg.project.xiirpl301112131.recyclerview3.model;
+
+import android.graphics.drawable.Drawable;
+
+
+public class Hotel {
+    public String judul;
+    public  String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul,String deskripsi,Drawable foto){
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
