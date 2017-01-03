67273563a98dafc2a6ec18100316c4a11bdab1ef
Vergie Ericson T <vergieericsont@gmail.com>
2017-01-03 19:32:40 +0700

    RecyclerView ADD

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl301112131/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl301112131/recyclerview3/MainActivity.java
@@ -21,49 +21,60 @@ import java.util.ArrayList;
 import id.sch.smktelkom_mlg.project.xiirpl301112131.recyclerview3.adapter.HotelAdapter;
 import id.sch.smktelkom_mlg.project.xiirpl301112131.recyclerview3.model.Hotel;
 
-public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter{
+public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter {
+
     public static final String HOTEL = "HOTEL";
+    public static final int REQUEST_CODE_ADD = 123;
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        mAdapter = new HotelAdapter(this,mList);
-        recyclerView.setAdapter(mAdapter);
-        fillData();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                goAdd();
+
             }
         });
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new HotelAdapter(this, mList);
+        recyclerView.setAdapter(mAdapter);
+
+        fillData();
+    }
+
+    private void goAdd() {
+        startActivityForResult(new Intent(this, InputActivity.class), REQUEST_CODE_ADD);
     }
 
     private void fillData() {
         Resources resources = getResources();
-        String [] arJudul = resources.getStringArray(R.array.places);
-        String [] arDeskripsi = resources.getStringArray(R.array.place_desc);
-        String [] arDetail = resources.getStringArray(R.array.place_details);
-        String [] arLokasi = resources.getStringArray(R.array.place_locations);
+        String[] arJudul = resources.getStringArray(R.array.places);
+        String[] arDeskripsi = resources.getStringArray(R.array.place_desc);
+        String[] arDetail = resources.getStringArray(R.array.place_details);
+        String[] arLokasi = resources.getStringArray(R.array.place_locations);
         TypedArray a = resources.obtainTypedArray(R.array.places_picture);
         String[] arFoto = new String[a.length()];
-        for(int i = 0; i < arFoto.length;i++){
-            int id = a.getResourceId(i,0);
-            arFoto[i] = ContentResolver.SCHEME_ANDROID_RESOURCE+"://"+resources.getResourcePackageName(id)+'/'+resources.getResourceTypeName(id)+'/'+resources.getResourceEntryName(id);
+        for (int i = 0; i < arFoto.length; i++) {
+            int id = a.getResourceId(i, 0);
+            arFoto[i] = ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
+                    + resources.getResourcePackageName(id) + '/'
+                    + resources.getResourceTypeName(id) + '/'
+                    + resources.getResourceEntryName(id);
         }
         a.recycle();
-        for (int i = 0; i <arJudul.length;i++){
-            mList.add(new Hotel(arJudul[i],arDeskripsi[i],arDetail[i],arLokasi[i],arFoto[i]));
+
+        for (int i = 0; i < arJudul.length; i++) {
+            mList.add(new Hotel(arJudul[i], arDeskripsi[i], arDetail[i], arLokasi[i], arFoto[i]));
         }
         mAdapter.notifyDataSetChanged();
     }
@@ -91,9 +102,19 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD && resultCode == RESULT_OK) {
+            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
+            mList.add(hotel);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
     public void doClick(int pos) {
-        Intent intent = new Intent(this,DetailActivity.class);
-        intent.putExtra(HOTEL,mList.get(pos));
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(HOTEL, mList.get(pos));
         startActivity(intent);
     }
 }
